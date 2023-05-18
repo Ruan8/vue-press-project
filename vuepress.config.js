411026e4b02ec51dc56ffa3db235ba26d443d70b
Ruan8 <1481434763@qq.com>
2023-05-19 00:14:23 +0800

    修改配置文件

--- vuepress.config.js
+++ vuepress.config.js
@@ -1,7 +1,11 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig, defaultTheme } from "vuepress";
 
 export default defineUserConfig({
     title: "vue-press-project",
     description: "vue-press-project",
     base: "/vue-press-project/", // github仓库名
+    sidebar: false,
+    theme: defaultTheme({
+        sidebar: false,
+    }),
 });
