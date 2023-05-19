c44a813d819c493b1c56e16410c6586ff593d38d
Ruan8 <1481434763@qq.com>
2023-05-19 12:33:00 +0800

    修改配置

--- vuepress.config.js
+++ vuepress.config.js
@@ -1,4 +1,5 @@
 import { defineUserConfig, defaultTheme } from "vuepress";
+import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 
 export default defineUserConfig({
     title: "vue-press-project",
@@ -12,8 +13,26 @@ export default defineUserConfig({
                 link: "/",
             },
         ],
+        repo: "Ruan8/vue-press-project",
+        docsDir: "docs",
+        editLinkText: "在 GitHub 上编辑此页",
         lastUpdatedText: "上次更新",
         contributorsText: "贡献者",
+        tip: "提示",
+        warning: "注意",
+        danger: "警告",
         backToHome: "回到首页",
+        notFound: [
+            "这里什么都没有",
+            "我们怎么到这来了？",
+            "这是一个 404 页面",
+            "看起来我们进入了错误的链接",
+        ],
     }),
+    plugins: [
+        googleAnalyticsPlugin({
+            id: "G-7M1XFXG9LR",
+            debug: true,
+        }),
+    ],
 });
